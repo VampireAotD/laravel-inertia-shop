37249ecaa3fb5cb1918efa31619d5066549c511e
Ilya Stepenko <38462532+VampireAotD@users.noreply.github.com>
2020-12-15 18:40:04 +0200

    Added recent views

---
+++ app/Console/Commands/Scheduler/RemoveFromRecentViews.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands\Scheduler;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class RemoveFromRecentViews extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'recent:remove';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Removes expired items from users recent views';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users = Redis::hKeys('users');
+
+        foreach ($users as $userList) {
+            $list = json_decode(Redis::hGet('users', $userList), true);
+
+            foreach ($list as $key => $item) {
+                if ($item['expire'] <= now()->timestamp) {
+                    unset($list[$key]);
+                }
+            }
+
+            Redis::hSet('users', $userList, json_encode(array_values($list)));
+        }
+    }
+}
---
+++ app/Http/Controllers/API/Frontend/RecentViewsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class RecentViewsController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        if (Redis::hExists('users', 'user:' . $request->ip())) {
+            $list = json_decode(Redis::hGet('users', 'user:' . $request->ip()), true);
+
+            usort($list, fn($current, $next) => $current['expire'] > $next['expire']);
+
+            return Product::find(collect($list)->pluck('id'));
+        }
+
+        return [];
+    }
+}
